fcf65eab7ca4e20ae1e9df87dacb9129270b921f
cyrosd <cyrilosdoit@gmail.com>
2015-05-09 16:15:32 +0200

    getters and setters

--- src/projet_hopital/TableauMedecin.java
+++ src/projet_hopital/TableauMedecin.java
@@ -20,15 +20,24 @@ public class TableauMedecin extends JScrollPane {
         ArrayList<Docteur> tabmed= new ArrayList<Docteur>();
     // création de liste ;
     // création de liste 
-        JTable med= new JTable(tabmed.size,5);
+        Object[][] donnee=new Docteur[tabmed.size()][5];
+         for (int i=0; i<tabmed.size();i++){
+             donnee[i][1]=tabmed.get(i).getNom();
+             donnee[i][2]=tabmed.get(i).getPrenom();
+             donnee[i][3]=tabmed.get(i).getAdresse();
+             donnee[i][4]=tabmed.get(i).getTel();
+             donnee[i][5]=tabmed.get(i).getSpecialite();
+             
+        }
+        /*JTable med= new JTable(tabmed.size(),5);
     for (int i=0; i<tabmed.size();i++){
         
         med.editCellAt(i,1,tabmed.get(i).getNom());
-        med.add(tabmed.get(i).getPrenom());
-        med.add(tabmed.get(i).getAdresse());
-        med.add(tabmed.get(i).getSpecialite();
-        
-    }
+        med.editCellAt(i,2,tabmed.get(i).getPrenom());
+        med.editCellAt(i,3,tabmed.get(i).getAdresse());
+        med.editCellAt(i,4,tabmed.get(i).getSpecialite());
+        med.editCellAt(i,5,tabmed.get(i).getTel());
+    }*/
     
 
 }
